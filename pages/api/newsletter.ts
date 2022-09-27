8b4859b2fe95e9e697f120f6f63b4d98c00009f6
Michał Dolny <michal.dolny@gmail.com>
2022-09-28 00:04:15 +0200

    Add MongoDB, store userEmail in the db

--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -1,10 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient } from "mongodb";
 
 interface Data {
   message: string;
 }
 
-export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -12,6 +13,13 @@ export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
       res.status(422).json({ message: "Invalid email address." });
       return;
     }
+
+    const client = await MongoClient.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.texpbg0.mongodb.net/?retryWrites=true&w=majority`
+    );
+    const db = client.db("newsletter");
+    await db.collection("emails").insertOne({ email: userEmail });
+    client.close();
 
     res.status(201).json({ message: `${userEmail} signed up.` });
   }
